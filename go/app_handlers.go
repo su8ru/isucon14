0f8484957b6335b41d1c2857bd6aa6c5b8aeb131
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2024-12-08 13:07:49 +0900

    latestStatus だけなしにした

--- go/app_handlers.go
+++ go/app_handlers.go
@@ -286,7 +286,13 @@ type executableGet interface {
 
 func getLatestRideStatus(ctx context.Context, tx executableGet, rideID string) (string, error) {
 	ctx = context.WithValue(ctx, "tx", tx)
-	return latestRideStatusCache.Get(ctx, rideID)
+
+	status := ""
+	if err := tx.GetContext(ctx, &status, `SELECT status FROM ride_statuses WHERE ride_id = ? ORDER BY created_at DESC LIMIT 1`, rideID); err != nil {
+		return "", err
+	}
+	return status, nil
+	// return latestRideStatusCache.Get(ctx, rideID)
 }
 
 func appPostRides(w http.ResponseWriter, r *http.Request) {
